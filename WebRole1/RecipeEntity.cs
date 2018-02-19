9f88efa0b6ac3874808b9713909db5ca368c4c90
Daniel <margalda@post.bgu.ac.il>
2018-02-19 12:02:58 +0200

    added table entity classes

--- WebRole1/RecipeEntity.cs
+++ WebRole1/RecipeEntity.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace WebRole1
 {
     // We want to store data in an Azure table. Each entity (row)
     // that we add requires a unique row key and a non-unique partition key.
     // These live in the base class TableServiceEntity from which we derive.
-    // We add the properties that we want to store in the table, in this case
-    // two strings called Name and Body. We also have to add our own code
-    // to generate a unique row key.
+    // We add the properties that we want to store in the table.
+    // We also have to add our own code to generate a unique row key.
 
     public enum Time
     {
@@ -37,34 +37,28 @@ namespace WebRole1
         Vietnamese,
     }
 
-
-    public class RecipeEntity : Microsoft.WindowsAzure.Storage.Table.TableEntity
+    public class RecipeEntity : TableEntity
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public double PreperationTime { get; set; }
-        public Time Time { get; set; }
-        public Kind Kind { get; set; }
-        public Cuisine Cuisine { get; set; }
-        public List<Ingredient> Ingredients { get; set; }
+        public string Time { get; set; }
+        public string Kind { get; set; }
+        public string Cuisine { get; set; }
 
-        public RecipeEntity(string name, string description, double preperationTime, Time time, Kind kind, Cuisine cuisine)
-        { 
+        public RecipeEntity() { }
+
+        public RecipeEntity(string name, string description, double preperationTime, string time, string kind, string cuisine)
+        {
             Name = name;
             Description = description;
             PreperationTime = preperationTime;
             Time = time;
             Kind = kind;
             Cuisine = cuisine;
-            Ingredients = new List<Ingredient>();
 
-            PartitionKey = cuisine.ToString();
-            RowKey = $"{DateTime.MaxValue.Ticks - DateTime.Now.Ticks:10}_{name}";
-        }
-
-        public void AddIngredients(Ingredient ingredient)
-        {
-            Ingredients.Add(ingredient);
+            PartitionKey = Cuisine;
+            RowKey = Name;
         }
     }
 }
---
+++ WebRole1/RecipeIngredientEntity.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace WebRole1
+{
+    public enum Measurement
+    {
+        Unit,
+        Teaspoon,
+        Cup,
+        Kg,
+    };
+
+    public enum Category
+    {
+        SaucesAndSpices,
+        MeatAndPasta,
+        SnacksAndCookies,
+        Baking,
+        Dairy,
+        FruitsAndVegetables
+    };
+
+    public class RecipeIngredientEntity : TableEntity
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Category { get; set; }
+        public string Measurement { get; set; }
+        public string RecipeName { get; set; }
+
+        public RecipeIngredientEntity() { }
+
+        public RecipeIngredientEntity(string name, double quantity, string category, string measurement, string recipeName)
+        {
+            Name = name;
+            Quantity = quantity;
+            Category = category;
+            Measurement = measurement;
+            RecipeName = recipeName;
+
+            PartitionKey = Category;
+            RowKey = $"{RecipeName}_{Name}";
+        }
+    }
+}
